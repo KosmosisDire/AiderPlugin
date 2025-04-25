4a9430dfc482c4a8fa67e9e0618a4ff5788605a5
Nathan George <kosmosisdire@gmail.com>
2025-04-25 13:59:14 -0400

    Fix finding package path

--- Editor/UnityAIUtils.cs
+++ Editor/UnityAIUtils.cs
@@ -1,6 +1,4 @@
-
-
-
+using UnityEditor.PackageManager;
 using System;
 using System.IO;
 using UnityEditor;
@@ -9,10 +7,10 @@ using UnityEngine;
 public static class UnityAIUtils
 {
     public static readonly string packageName = "com.kosmosisdire.unity-ai";
-
     public static string GetPath(string path)
     {
-        var newPath = Path.Combine($"Packages/{packageName}/Editor", path);
+        var newPath = Path.Combine($"{UnityEditor.PackageManager.PackageInfo.FindForPackageName(packageName).resolvedPath}/Editor", path);
+        Debug.Log($"Package Name: {newPath}");
         return newPath;
     }
 }
